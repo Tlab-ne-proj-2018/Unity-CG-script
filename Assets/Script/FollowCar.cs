50062a3b6411638016b6d3dc5dbeb502fe6bb587
Yasu-2 <44080915+Yasu-2@users.noreply.github.com>
2018-10-12 16:21:37 +0900

    initial commit

---
+++ Assets/Script/FollowCar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCar : MonoBehaviour {
+
+    public Transform target;    // ターゲット(自動車)への参照
+    private Vector3 offset_position;     // 相対座標
+    private Quaternion rotation_matrix;  // 回転座標
+    private Vector3 camera_position;
+    private void Start()
+    {
+        // 自分自身とtargetの相対距離を求める オフセット = 自動車の座標 - カメラの座標
+        offset_position = GetComponent<Transform>().position - target.position;
+    }
+
+    private void Update()
+    {
+        // 自動車の回転角度を求める
+        rotation_matrix = target.rotation;
+        camera_position= rotation_matrix * offset_position;
+
+
+        // 自分自身の座標に、targetの座標に相対座標を足した値を設定する
+        GetComponent<Transform>().position = target.position + camera_position;
+        GetComponent<Transform>().rotation = target.rotation;
+    }
+}
---
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CalcDistance.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// レーザー距離計
+namespace UnityStandardAssets.Vehicles.Car
+{
+    public class CalcDistance : MonoBehaviour
+    {
+        
+        public const float NOTHING = -1;    // 計測不能
+
+        public float maxDistance = 30;      // 計測可能な最大距離
+        public float distance = 1.0f;              // 計測距離
+
+        // 距離計測
+        public void Update()
+        {
+            // 前方ベクトル計算
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+            // 距離計算
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, fwd, out hit, maxDistance))
+            {
+                distance = hit.distance;
+                Debug.Log(distance);
+            }
+            else
+            {
+                distance = NOTHING;
+            }
+        }
+    }
+}
